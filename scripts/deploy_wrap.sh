6e73867f43a59dcf959f88f16c6649b449c2a95e
森郁 <senyu@mail.dlut.edu.cn>
2015-09-30 16:36:18 +0800

    chmod +x 3

--- scripts/deploy_wrap.sh
+++ scripts/deploy_wrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export NEWVERSION=$(git rev-parse --short HEAD);
-export DEPLOYCOPY="~/DEPLOY";
-export DEPLOYPATH="";
+export NEWVERSION=$(git rev-parse --short HEAD)
+export DEPLOYCOPY="~/DEPLOY"
+export DEPLOYPATH="/home/www/py/senyu.me/deploy/"
 
-echo $NEWVERSION;
+echo $NEWVERSION
 
 tar -zcvf ../deploy_$NEWVERSION.tar.gz * 
 
 scp -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ../deploy_$NEWVERSION.tar.gz root@senyu.me:$DEPLOYCOPY
-ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 root@senyu.me "./scripts/remote_excute.sh $NEWVERSION $DEPLOYPATH $DEPLOYCOPY"
+ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 root@senyu.me ".remote_excute.sh $NEWVERSION $DEPLOYPATH $DEPLOYCOPY"
