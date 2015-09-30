8c0e4c365c33cd87455b2a338e46e5c3e1f17374
森郁 <senyu@mail.dlut.edu.cn>
2015-09-30 16:33:06 +0800

    chmod +x 2

--- scripts/deploy_wrap.sh
+++ scripts/deploy_wrap.sh
@@ -6,7 +6,7 @@ export DEPLOYPATH="";
 
 echo $NEWVERSION;
 
-tar -zcvf ./deploy_$NEWVERSION.tar.gz * 
+tar -zcvf ../deploy_$NEWVERSION.tar.gz * 
 
-scp -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ./deploy_$NEWVERSION.tar.gz root@senyu.me:$DEPLOYCOPY
+scp -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ../deploy_$NEWVERSION.tar.gz root@senyu.me:$DEPLOYCOPY
 ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 root@senyu.me "./scripts/remote_excute.sh $NEWVERSION $DEPLOYPATH $DEPLOYCOPY"
