7f64b1997967b22708b41a1fa5cf68cb9134f35c
森郁 <senyu@mail.dlut.edu.cn>
2015-10-01 21:12:19 +0800

    [Fix] 修改多服务器环境 准备测试一下

---
+++ scripts/deploy_production.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "enter deploy_wrap"
+
+DEPLOYPROJECTNAME="nserverPy"
+NEWVERSION=$(git rev-parse --short HEAD)
+DEPLOYCOPY="~/DEPLOY/"
+DEPLOYDEST="/home/wwwroot/py.senyu.me/deploy/"
+DEPLOYFILENAME=deploy_${DEPLOYPROJECTNAME}_${NEWVERSION}.tar.gz
+DEPLOYSERVER="root@senyu.me"
+DEPLOYMODE="PRODUCTION"
+
+echo $DEPLOYPROJECTNAME
+echo $NEWVERSION
+echo $DEPLOYCOPY
+echo $DEPLOYDEST
+echo $DEPLOYFILENAME
+echo $DEPLOYSERVER
+
+tar -zcf ../$DEPLOYFILENAME * --exclude=venv --exclude=scripts
+
+scp -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ../${DEPLOYFILENAME} ${DEPLOYSERVER}:${DEPLOYCOPY}
+#ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 $DEPLOYSERVER "ls -l $DEPLOYCOPY;"
+ssh -i ~/.ssh/id_rsa  -o StrictHostKeyChecking=no -p 22 ${DEPLOYSERVER} "${DEPLOYCOPY}/remote_execute.sh ${NEWVERSION} ${DEPLOYCOPY} ${DEPLOYDEST} ${DEPLOYPROJECTNAME} ${DEPLOYFILENAME} ${DEPLOYMODE}"
