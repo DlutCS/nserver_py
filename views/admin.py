580fedcd5f45e3e3e08eb2e4196920f46635285d
SenYu@ThinkMac <senyu@mail.dlut.edu.cn>
2015-10-12 00:31:17 +0800

    [Feature] 加入WTForms验证库

--- views/admin.py
+++ views/admin.py
@@ -4,15 +4,25 @@ from flask.views import MethodView
 from flask import url_for
 from flask import redirect, render_template, request
 from models.user import User
-
+from models.form import LoginForm
 
 class LoginView(MethodView):
 
     def get(self):
-        return "Login here"
+        form = LoginForm()
+        return render_template('login.html', **locals())
 
     def post(self):
-        return redirect(url_for("api.login"), code=307)
+        #api.login
+        #return redirect(url_for("api.login"), code=307)
+
+        form = LoginForm(request.form)
+        if form.validate():
+            # TODO:  add login session 
+            return redirect('/')
+        else:
+            # return error
+            return render_template('login.html', **locals())
 
 
 class LogoutView(MethodView):
