2abe5bb65c9a038e58e66d154af844587324b2c8
linwei <yimulinwei@gmail.com>
2015-10-12 22:29:12 +0800

    [Fix]category

--- views/home.py
+++ views/home.py
@@ -5,6 +5,7 @@ from flask import redirect, render_template, request
 from models.category import Category
 from models.news import News
 from models.form import LoginForm
+from flask import abort, url_for
 
 
 
@@ -17,3 +18,18 @@ class HomeView(MethodView):
         form = LoginForm()
         
         return render_template('index.html', **locals())
+
+
+class HomeCategoryView(MethodView):
+
+    def get(self, cid):
+        if not cid or not Category.get(cid):
+            abort(404)
+        if cid == 1:
+            return redirect(url_for('main.home'))
+        news_header = News.get_by_category(cid, order='create_time', start=0, limit=7)
+        news_latest = News.get_by_category(cid, order='create_time', start=7)
+        news_popular = News.get_by_category(cid, order='comment_count', start=0)
+        form = LoginForm()
+        return render_template('index.html', **locals())
+        
