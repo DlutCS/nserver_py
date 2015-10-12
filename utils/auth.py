4b69c411d5f1a664d7eadbc3924bf813c411e13d
linwei <yimulinwei@gmail.com>
2015-10-12 10:06:05 +0800

    [Fix]news忘记加create_time了

--- utils/auth.py
+++ utils/auth.py
@@ -1,12 +1,15 @@
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from models.user import User
-import os
 from appins import app
 
+app.secret_key = 'super secret key'
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = "main.login"
 oid = OpenID(app, '/tmp')
+
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @lm.user_loader
 def load_user(id):
