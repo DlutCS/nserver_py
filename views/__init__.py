5999a8b551f26ba9fa837c0f99cfa1ac9fd28804
SenYu@ThinkMac <senyu@mail.dlut.edu.cn>
2015-10-15 19:20:11 +0800

    [Feature]admin登陆callback跳转

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from views.admin import LoginView, LogoutView, RegisterView
+from views.admin import LoginView, LogoutView, RegisterView, AdminIndexView
 from views.home import HomeView, HomeCategoryView, HomeNewsView
 
 main = Blueprint('main', __name__)
@@ -12,6 +12,7 @@ def page_not_found(error):
 main.add_url_rule('/', view_func=HomeView.as_view('home'), methods=['GET'])
 
 # admin
+main.add_url_rule('/admin/', view_func=AdminIndexView.as_view('admin'), methods=['GET'])
 main.add_url_rule('/register/', view_func=RegisterView.as_view('register'), methods=['GET', 'POST'])
 main.add_url_rule('/login/', view_func=LoginView.as_view('login'), methods=['GET', 'POST'])
 main.add_url_rule('/logout/', view_func=LogoutView.as_view('logout'), methods=['GET'])
--- views/admin.py
+++ views/admin.py
@@ -5,23 +5,37 @@ from flask import url_for
 from flask import redirect, render_template, request
 from models.user import User
 from models.forms import LoginForm, RegisterForm
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from flask import flash
 
+
+class AdminIndexView(MethodView):
+
+    def get(self):
+        if current_user.is_authenticated:
+            return render_template('admin/index.html')
+        else:
+            return '<script>window.location.href="/login?callback=/admin/"+window.location.hash.replace(/^#/,"%23")</script>';
 
 class LoginView(MethodView):
 
     def get(self):
         loginform = LoginForm()
+        callback = request.args.get('callback','/')
+
         return render_template('login.html', **locals())
 
     def post(self):
         loginform = LoginForm(request.form)
+        callback = request.form.get('callback','/')
+
         if loginform.validate():
             # add login session 
             print u'Successfully logged in as %s' % loginform.user.username
             login_user(loginform.user, remember=loginform.remember.data)
-            return redirect('/')
+
+            return redirect(callback)
+
         else:
             # return error
             return render_template('login.html', **locals())
